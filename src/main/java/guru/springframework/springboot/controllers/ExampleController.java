f43bf84e1a1d4e47b87a0edd49f02a3d4a8a5936
Wolf Developer <lupo.andrea.93@gmail.com>
2020-11-12 19:09:45 +0100

    Aggiunta creazione info tramite spring-boot-maven-plugin

---
+++ src/main/java/guru/springframework/springboot/controllers/ExampleController.java
@@ -0,0 +1,20 @@
+package guru.springframework.springboot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ExampleController {
+
+    @GetMapping
+    public Map<String, Object> getIndex() {
+        Map<String, Object> returnVal = new HashMap<>();
+
+        returnVal.put("hello", "world");
+
+        return returnVal;
+    }
+}
